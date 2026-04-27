a638ba5a16ce986049cef048da595e417098d1ef
smedWolf26 <smedina.lambda@gmail.com>
2026-04-27 09:58:29 -0500

    added pinia to main.js

--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
 import { createApp } from 'vue'
+import { createPinia } from 'pinia'
 import '@/assets/style.css'
 import router from '@/router'
 import App from '@/App.vue'
 
+const app = createApp(App)
 createApp(App).use(router).mount('#app')
+app.use(createPinia())
